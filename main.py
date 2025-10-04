ec92c326e4a78578e67b63fc64d6793a226d614e
xurshidjumayev50-beep <xurshidjumayev@gmail.com>
2025-10-04 22:10:51 +0500

    Optimal

--- main.py
+++ main.py
@@ -1,49 +1,21 @@
 
 from fastapi import FastAPI
+from typing import Optional   # <-- MUHIM: shu joyda import qilinadi
 
 app = FastAPI()
 
 @app.get('/')
-
-
-
 def index():
-
-
     return {'data': 'blog list'}
 
-
 @app.get('/blog')
-
-
-def index(limit=10, published: bool = True, sort: Optional[str] = None):
-
-
+def get_blogs(limit: int = 10, published: bool = True, sort: Optional[str] = None):
     # only get 10 published blogs
-
-
     if published:
-
-
         return {'data': f'{limit} published blogs from the db'}
-
-
     else:
-
-
         return {'data': f'{limit} blogs from the db'}
 
-
-
-
-
-
-
-
 @app.get('/blog/unpublished')
-
-
 def unpublished():
-
-
     return {'data': 'all unpublished blogs'}
