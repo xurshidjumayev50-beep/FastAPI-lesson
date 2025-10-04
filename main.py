e6e4d877dc006c78a14eb4873ca907c790971381
xurshidjumayev50-beep <xurshidjumayev@gmail.com>
2025-10-04 16:31:29 +0500

    upda t 3

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 from fastapi import FastAPI
 
+
+
+
+
+
 app = FastAPI()
+
+
+
+
+
+
+
 
 @app.get('/')
 
 
-
 def index():
-    return {'data': {'name':'Sarthak'}}
+
+
+    return {'data': {'name': 'Sarthak'}}
+
+
+
+
+
+
+
+
+@app.get('/about')
+
+
+def about():
+
+
+    return {'data': 'about page'}
