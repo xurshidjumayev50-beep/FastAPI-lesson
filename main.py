f0b7ee1fdcffa632ec236fcd1517683c85474f83
xurshidjumayev50-beep <xurshidjumayev@gmail.com>
2025-10-04 21:50:25 +0500

    blog id

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':'blog list'}
+@app.get("/blog/{blog_id}")
+def show(blog_id: int):
+    
+    return {"data": {"id": blog_id}}
 
-
-@app.get('/about')
-def abc():
-    return {'data': 'about page'}
+@app.get("/blog/{blog_id}/comments")
+def comments(blog_id: int):
+    
+    return {"data": ["1", "2"]}
