35607db71c87cac0e6aa53b4ef314d2b4c24c18a
xurshidjumayev50-beep <xurshidjumayev@gmail.com>
2025-10-07 16:00:09 +0500

    id

--- blog  /main.py
+++ blog  /main.py
@@ -1,51 +1,18 @@
-from fastapi import FastAPI
-
-
-
 from fastapi import FastAPI, Depends
+
+
+
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 
 from . import schemas, models
 
-
-from .database import engine
-
-
 from .database import engine, SessionLocal
-
 
 from sqlalchemy.orm import Session
 
-
-
-
-app = FastAPI()
-
-
-
-models.Base.metadata.create_all(engine)
-
-
-
-
-
-
-def get_db():
-
-
-    db = SessionLocal()
-
-
-    try:
-
-
-        yield db
-
-
-    finally:
-
+@@ -16,7 +16,7 @@ def get_db():
 
         db.close()
-
 
 
 
@@ -55,54 +22,17 @@ def get_db():
 @app.post('/blog')
 
 
-def create(request: schemas.Blog):
-
-
-    return request
-
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
-
     new_blog = models.Blog(title=request.title, body=request.body)
-
 
     db.add(new_blog)
 
-
-    db.commit()
-
-
-    db.refresh(new_blog)
-
-
-    return new_blog
- db.commit()
-
-
-    db.refresh(new_blog)
-
-    return new_blog
-
-
-
-
-
-
-
-
-@app.get('/blog')
-
-
-def all(db: Session = Depends(get_db)):
-
-
-    blogs = db.query(models.Blog).all()
-
+@@ -31,7 +31,13 @@ def all(db: Session = Depends(get_db)):
 
     return blogs
-
-
 
 
 
@@ -115,10 +45,29 @@ def all(db: Session = Depends(get_db)):
 def show(id, db: Session = Depends(get_db)):
 
 
+@app.get('/blog/{id}', status_code=200)
+
+
+def show(id, response: Response, db: Session = Depends(get_db)):
+
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
 
 
+    if not blog:
+
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+
+
+                            detail=f"Blog with the id {id} is not available")
+
+
+        # response.status_code = status.HTTP_404_NOT_FOUND
+
+
+        # return {'detail': f"Blog with the id {id} is not available"}
+
+
+
+
     return blog
-
-
-
