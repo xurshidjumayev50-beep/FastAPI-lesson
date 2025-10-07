d61f7abe4033f90dc267f9ca2c51d2da6375d86a
xurshidjumayev50-beep <xurshidjumayev@gmail.com>
2025-10-07 16:19:43 +0500

    status

--- blog  /main.py
+++ blog  /main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Depends
-
+from typing import List
 
 
 from fastapi import FastAPI, Depends, status, Response, HTTPException
@@ -8,77 +7,38 @@ from . import schemas, models
 
 from .database import engine, SessionLocal
 
-from sqlalchemy.orm import Session
-
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
-
-
-def destroy(id, db: Session = Depends(get_db)):
-
-
-    db.query(models.Blog).filter(models.Blog.id ==
-
-
-                                 id).delete(synchronize_session=False)
-
-
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-
-
-
-
-
-    if not blog.first():
-
-
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-
-
-                            detail=f"Blog with id {id} not found")
-
-
-
-
-
-    blog.delete(synchronize_session=False)
-
-    db.commit()
-
-    return 'done'
-
-
-
-
-
-
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
-
-
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-
-
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-
-
-
-
-
-    if not blog.first():
-
-
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-
-
-                            detail=f"Blog with id {id} not found")
-
-
-
-
-
-    blog.update(request)
-
-
-    db.commit()
-
+@@ -51,13 +52,13 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
 
     return 'updated'
+
+
+
+
+
+
+@app.get('/blog')
+
+
+@app.get('/blog', response_model=List[schemas.ShowBlog])
+
+def all(db: Session = Depends(get_db)):
+
+    blogs = db.query(models.Blog).all()
+
+    return blogs
+
+
+
+
+
+
+@app.get('/blog/{id}', status_code=200)
+
+
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
+
+def show(id, response: Response, db: Session = Depends(get_db)):
+
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    if not blog:
