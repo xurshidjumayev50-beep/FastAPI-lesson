643b1f0e8f435e183b957d9802bf3f71db05375f
xurshidjumayev50-beep <xurshidjumayev@gmail.com>
2025-10-07 13:00:09 +0500

    BaseModel

--- blog  /main.py
+++ blog  /main.py
@@ -1,6 +1,12 @@
 from fastapi  import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
+class Blog(Model):
+    title:str
+    body:str
 
 @app.post('/blog')
-def create():
+def create(requset Blog):
+    return requset
