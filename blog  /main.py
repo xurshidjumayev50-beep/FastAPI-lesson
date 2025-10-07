78b26891de8d51f1c60463fe19b1b7cd1de65087
xurshidjumayev50-beep <xurshidjumayev@gmail.com>
2025-10-07 17:01:20 +0500

    user password

--- blog  /main.py
+++ blog  /main.py
@@ -1,48 +1,24 @@
-from typing import List
-
-
-from fastapi import FastAPI, Depends, status, Response, HTTPException
-
 from . import schemas, models
+
 
 from .database import engine, SessionLocal
 
-@@ -51,13 +52,13 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-
-    return 'updated'
+from sqlalchemy.orm import Session
 
 
-
+from .hashing import Hash
 
 
 
-@app.get('/blog')
-
-
-@app.get('/blog', response_model=List[schemas.ShowBlog])
-
-def all(db: Session = Depends(get_db)):
-
-    blogs = db.query(models.Blog).all()
-
-    return blogs
+app = FastAPI()
 
 
 
+@@ -70,9 +71,12 @@ def show(id, response: Response, db: Session = Depends(get_db)):
+
+    return blog
 
 
-
-@app.get('/blog/{id}', status_code=200)
-
-
-@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
-
-def show(id, response: Response, db: Session = Depends(get_db)):
-
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-
-    if not blog:
-        return blog
 
 
 
@@ -54,20 +30,19 @@ def show(id, response: Response, db: Session = Depends(get_db)):
 
 @app.post('/user')
 
-
 def create_user(request: schemas.User,db: Session = Depends(get_db)):
 
 
     new_user = models.User(name=request.name,email=request.email,password=request.password)
 
 
-    db.add(new_user)
 
+
+
+    new_user = models.User(name=request.name,email=request.email,password=Hash.bcrypt(request.password))
+
+    db.add(new_user)
 
     db.commit()
 
-
     db.refresh(new_user)
-
-
-    return new_user
