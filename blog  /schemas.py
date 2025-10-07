7edb75247514fcd4b82ef989747fff579096e254
xurshidjumayev50-beep <xurshidjumayev@gmail.com>
2025-10-07 16:26:53 +0500

    Showblog

--- blog  /schemas.py
+++ blog  /schemas.py
@@ -8,5 +8,25 @@ class Blog(BaseModel):
 
     title: str
 
+    body: str
+
+
+
+
+
+class ShowBlog(BaseModel):
+
+
+    title: str
+
 
     body: str
+
+
+
+
+
+    class Config():
+
+
+        orm_mode = True
