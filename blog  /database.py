0dd22b300ee5c863aad8b13d4b1878aca7d8609e
xurshidjumayev50-beep <xurshidjumayev@gmail.com>
2025-10-07 14:43:11 +0500

    engina

--- blog  /database.py
+++ blog  /database.py
@@ -1,3 +1,9 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_bas
+from sqlalchemy.orm import sessionmaker
+SQLALCHAMY_DATAASE_URL = 'sqlite:///./blog.db'
 
-engina=create_engine('sqlite:///./blog.db:memory:',echo=True)
+engina=create_engine(SQLALCHAMY_DATAASE_URL,connect_arggs={"check_same_threread":False})
+SessionLocal =sessionmaker(bind=engina,autocommit=False,autoflush=False)
+
+Base=declarative_bas()
