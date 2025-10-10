83fb4dcf4fcd098ae67560b55c0df4a106b1c700
xurshidjumayev50-beep <xurshidjumayev@gmail.com>
2025-10-10 17:47:49 +0500

    pasword hash

--- blog  /hashing.py
+++ blog  /hashing.py
@@ -5,12 +5,6 @@ from passlib.context import CryptContext
 
 
 
-pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-
-
-
 class Hash():
 
 
@@ -18,3 +12,15 @@ class Hash():
 
 
         return pwd_cxt.hash(password)
+
+
+        return pwd_cxt.hash(password)
+
+
+
+
+
+    def verify(hashed_password,plain_password):
+
+
+        return pwd_cxt.verify(plain_password,hashed_password)
