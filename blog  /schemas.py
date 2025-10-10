c348dff05279ddab906155667fbabfdbc66f33ff
xurshidjumayev50-beep <xurshidjumayev@gmail.com>
2025-10-10 16:39:59 +0500

    list

--- blog  /schemas.py
+++ blog  /schemas.py
@@ -1,35 +1,82 @@
 
 
+from typing import List
+
+
 from pydantic import BaseModel
 
 
-class User(BaseModel):
-
-
-    name:str
-
-    email:str
-
-
-    password:str
-
-
-    password:str
 
 
 
 
+class Blog(BaseModel):
+
+
+    title: str
+
+
+    body: str
+
+
+
+
+
+class ShowBlog(BaseModel):
+
+
+class BlogBase(BaseModel):
+
+    title: str
+
+    body: str
+
+
+
+
+class Blog(BlogBase):
+
+    class Config():
+
+        orm_mode = True
+
+
+
+@@ -20,5 +18,14 @@ class User(BaseModel):
 
 class ShowUser(BaseModel):
 
-
     name:str
 
-
     email:str
+
+
+    blogs : List[Blog] =[]
 
 
     class Config():
 
+
+        orm_mode = True
+
+
+
+
+
+class ShowBlog(BaseModel):
+
+
+    title: str
+
+
+    body:str
+
+
+    creator: ShowUser
+
+
+
+
+    class Config():
 
         orm_mode = True
