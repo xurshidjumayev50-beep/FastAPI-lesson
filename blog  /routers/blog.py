c6bdbb230fd6c8c729a30b4f54eb0b7289b7f40a
xurshidjumayev50-beep <xurshidjumayev@gmail.com>
2025-10-10 16:59:18 +0500

    ApIRouter

--- blog  /routers/blog.py
+++ blog  /routers/blog.py
@@ -1,23 +1,32 @@
-ffrom fastapi import APIRouter
+from fastapi import APIRouter,Depends,status,HTTPException
 
 
-
-from .. import database, schemas, models
-
+from .. import schemas, database, models
 
 from sqlalchemy.orm import Session
 
 
-from fastapi import APIRouter,Depends,status,HTTPException
-
-
-from ..hashing import Hash
-
-
-
-
-
 router = APIRouter()
+
+
+
+
+
+
+
+
+router = APIRouter(
+
+
+    prefix="/blog",
+
+
+    tags=['Blogs']
+
+
+)
+
+
 
 
 get_db = database.get_db
@@ -25,50 +34,90 @@ get_db = database.get_db
 
 
 
+@router.get('/blog', response_model=List[schemas.ShowBlog],tags=['blogs'])
+
+
+@router.get('/', response_model=List[schemas.ShowBlog])
+
+def all(db: Session = Depends(get_db)):
+
+    blogs = db.query(models.Blog).all()
+
+    return blogs
 
 
 
 
-@router.post('/user', response_model=schemas.ShowUser,tags=['users'])
 
 
-def create_user(request: schemas.User,db: Session = Depends(get_db)):
+@router.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
 
 
-    new_user = models.User(name=request.name,email=request.email,password=Hash.bcrypt(request.password))
+@router.post('/', status_code=status.HTTP_201_CREATED)
 
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
-    db.add(new_user)
+    new_blog = models.Blog(title=request.title, body=request.body,user_id=1)
 
+    db.add(new_blog)
 
     db.commit()
 
+    db.refresh(new_blog)
 
-    db.refresh(new_user)
-
-
-    return new_user
+    return new_blog
 
 
 
 
-
-@router.get('/user/{id}',response_model=schemas.ShowUser,tags=['users'])
-
-
-def get_user(id:int,db: Session = Depends(get_db)):
+@router.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT,tags=['blogs'])
 
 
-    user = db.query(models.User).filter(models.User.id == id).first()
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+
+def destroy(id, db: Session = Depends(get_db)):
+
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
 
 
-    if not user:
+
+@@ -32,7 +38,7 @@ def destroy(id, db: Session = Depends(get_db)):
+
+    return 'done'
 
 
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
 
 
-                            detail=f"User with the id {id} is not available")
 
 
-    return user
+@router.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED,tags=['blogs'])
+
+
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
+
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+
+
+@@ -45,7 +51,7 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+
+    return 'updated'
+
+
+
+
+
+
+@router.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog,tags=['blogs'])
+
+
+@router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
+
+def show(id, db: Session = Depends(get_db)):
+
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    if not blog:
+
